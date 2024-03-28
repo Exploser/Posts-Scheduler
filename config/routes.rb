72ffa6044d53d32c4457631fb3f624402fd28d27
Exploser <exploserbusiness@gmail.com>
2024-03-27 23:33:46 -0400

    Add Sign-up page and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # GET /about
   get "about", to: "about#index", as: :about
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  #get "up" => "rails/health#show", as: :rails_health_check
+  # GET /sign_up
+  get "sign_up", to: "registrations#new"
 
+# Defines the root path route ("/")
   root to: "main#index"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
