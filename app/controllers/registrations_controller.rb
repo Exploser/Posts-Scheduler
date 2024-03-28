a1af16e759a7538a880a124f22dad179ca7f80a9
Exploser <exploserbusiness@gmail.com>
2024-03-28 00:47:58 -0400

    Link sign_up form with the databse

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,17 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    render plain: "Thanks!"
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Successfully created account"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
