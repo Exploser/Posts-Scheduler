e4231bc888d68cd4ced20f5d6fe763b9f2ca2d94
Exploser <exploserbusiness@gmail.com>
2024-03-29 16:55:08 -0400

    Add feature to edit passwords

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # /about
   get "about", to: "about#index", as: :about
 
+  # /password
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   # /sign_up
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
@@ -11,6 +15,7 @@ Rails.application.routes.draw do
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
 
+  # /log_out
   delete "logout", to: "sessions#destroy"
 
 # Defines the root path route ("/")
