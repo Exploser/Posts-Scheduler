818322a3b79bb42a2a73d2668341edee78bea321
Exploser <exploserbusiness@gmail.com>
2024-03-31 16:41:59 -0400

    Add edit function to handle password edits Remove new function

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,16 +1,20 @@
 class PasswordResetsController < ApplicationController
-  def new
-
-  end
 
   def create
     @user = User.find_by(email: params[:email])
 
     if @user.present?
+
       #send email
         PasswordResetMailer.with(user: @user).reset.deliver_later  # Change to deliver_now to send the email faster
     end
 
     redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset your password"
+  end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: "Expired Token"
   end
 end
