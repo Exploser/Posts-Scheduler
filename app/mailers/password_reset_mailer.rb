8a85dc302192d9a108e3d29c7ff8f005a44fd232
Exploser <exploserbusiness@gmail.com>
2024-03-31 15:20:55 -0400

    Add token generation for password_reset_mailer, to send users email to reset their password

--- app/mailers/password_reset_mailer.rb
+++ app/mailers/password_reset_mailer.rb
@@ -1,2 +1,8 @@
 class PasswordResetMailer < ApplicationMailer
+
+  def reset
+    @token = params[:user].signed_id(purpose: "password_reset", expires_in: 15.minutes)
+
+    mail to: params[:user].email
+  end
 end
