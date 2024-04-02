d3922dc16fecc25f26a26dde26e51ddde46a1bb8
Exploser <exploserbusiness@gmail.com>
2024-04-02 09:54:56 -0400

    Add password update controller + password edit view

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -17,4 +17,19 @@ class PasswordResetsController < ApplicationController
   rescue ActiveSupport::MessageVerifier::InvalidSignature
     redirect_to sign_in_path, alert: "Expired Token"
   end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "Your password was reset successfully"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
