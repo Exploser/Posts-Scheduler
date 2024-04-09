9746e7338ace8f3fcadb733d7a269a16ced7734c
Exploser <exploserbusiness@gmail.com>
2024-04-09 01:06:17 -0400

    Implement feature for saving tweets

--- app/controllers/twitter_account_controller.rb
+++ app/controllers/twitter_account_controller.rb
@@ -1,13 +1,17 @@
 class TwitterAccountController < ApplicationController
   before_action :require_user_logged_in
+  before_action :set_twitter_account, only: [:destroy]
 
   def index
     @twitter_account = Current.user.twitter_account
   end
 
   def destroy
-    @twitter_account = Current.user.twitter_account.find(params[:id])
     @twitter_account.destroy
     redirect_to twitter_account_index_path, notice: "Successfully Deleted @#{@twitter_account.username}"
+  end
+
+  def set_twitter_account
+    @twitter_account = Current.user.twitter_account.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :twitter_account
+  has_many :tweets
+
   has_secure_password
 
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
