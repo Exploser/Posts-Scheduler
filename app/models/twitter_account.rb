3f7bd927d98afec0a5cb6044195d2973796d2df9
Exploser <exploserbusiness@gmail.com>
2024-04-12 14:52:44 -0400

    Add new method to get user creds with the X gem

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -1,5 +1,17 @@
 class TwitterAccount < ApplicationRecord
   belongs_to :user
+  require "x"
   has_many :tweets
+
   validates :username, uniqueness: true
+
+  def client
+    x_credentials = {
+    api_key: Rails.application.credentials.dig(:twitter, :api_key),
+    api_key_secret: Rails.application.credentials.dig(:twitter, :api_secret_key),
+    access_token: token,
+    access_token_secret: secret
+    }
+    X::Client.new(**x_credentials)
+  end
 end
