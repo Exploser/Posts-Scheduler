4a9260b85515b48df3e3ec13f6640d8d1b60aba3
Exploser <exploserbusiness@gmail.com>
2024-04-12 14:51:25 -0400

    Add function to publish tweets according to the schedule time

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :require_user_logged_in
+  before_action :set_tweet, only: [:edit, :update, :destroy]
 
   def index
     @tweets = Current.user.tweets
@@ -9,18 +10,41 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
 
+  # TweetJob.set(wait_until: @tweet.publish_at).perform_later(@tweet)
   def create
     @tweet = Current.user.tweets.new(tweet_params)
     if @tweet.save
+      TweetJob.perform_later(@tweet)
       redirect_to tweets_path, notice: "Tweet was Scheduled Successfully"
     else
       render :new
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      TweetJob.set(wait_until: @tweet.publish_at).perform_later(@tweet)
+      redirect_to tweets_path, notice: "Tweet was updated successfully"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: "Tweet was deleted successfully"
+  end
+
   private
 
   def tweet_params
     params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
+
+  def set_tweet
+    @tweet = Current.user.tweets.find(params[:id])
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :twitter_account
+  require "x"
 
   validates :body, length: { minimum: 1, maximum: 280 }
   validates :publish_at, presence: true
@@ -9,8 +10,18 @@ class Tweet < ApplicationRecord
     self.publish_at ||= 24.hours.from_now
   end
 
+  after_save_commit do
+    if publish_at_previously_changed?
+      TweetJob.set(wait_until: publish_at).perform_later(self)
+    end
+  end
+
   def published?
     tweet_id?
   end
 
+  def publish_to_twitter!
+    tweet = twitter_account.client.post("tweets", "{\"text\":\"#{body}\"}")
+    update(tweet_id: tweet["data"]["id"])
+    end
 end
