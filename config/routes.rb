808ea58c3298525eda770a7b1eb1e54eb733acc1
Exploser <exploserbusiness@gmail.com>
2024-04-12 14:50:01 -0400

    Fix typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   delete "logout", to: "sessions#destroy"
 
   # /Omni_Auth
-  post "auth/twitter/callback", to: "omniauth_callbacks#twitter2"
+  post "auth/twitter2/callback", to: "omniauth_callbacks#twitter2"
   get "auth/twitter2/callback", to: "omniauth_callbacks#twitter2"
 
 
